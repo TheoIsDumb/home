e8699b32463e97a8351f31b69c02caf5b8503557
theo <lesgooo1993@proton.me>
2024-05-29 01:12:43 +0530

    disabled rotating the gltf; orbitcontrols autorotate=true, pc.glb laptop red color is less sharp

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -2,20 +2,18 @@
   import { T, useTask } from '@threlte/core'
   import { OrbitControls } from '@threlte/extras'
 	import { useGltf } from '@threlte/extras'
-
-	let rotation = 0
-  useTask((delta) => {
-    rotation += delta/10
-  })
 </script>
 
 <T.PerspectiveCamera
   makeDefault
-	position.x={-20}
-	position.y={20}
-	position.z={80}
+	position={[-25, 25, 45]}
 	>
-  <OrbitControls enableDamping />
+  <OrbitControls
+		enableDamping
+		autoRotate={true}
+		minDistance={50}
+		maxDistance={100}
+	/>
 </T.PerspectiveCamera>
 
 <T.DirectionalLight color="white" intensity={5} position={[10, 20, 20]}/>
@@ -26,6 +24,5 @@
 		position={[0, 0, 0]}
 		castShadow
 		receiveShadow
-		rotation.y={rotation}
 	/>
 {/await}
