39d2aba9a6646b3985049e876573a0a45b6bd955
theo <lesgooo1993@proton.me>
2024-05-29 15:19:07 +0530

    removed scene.svelte (enthinn verthe?) moved it all inside app.svelte; pc.blend changes

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,8 +1,28 @@
 <script>
   import { Canvas } from '@threlte/core'
-  import Scene from './Scene.svelte'
+
+	import { T } from '@threlte/core'
+	import { OrbitControls } from '@threlte/extras'
+	import PC from './pc.svelte'
+
+	import { orbitRotate } from '$lib/store'
 </script>
 
 <Canvas>
-  <Scene />
+  <T.PerspectiveCamera
+    makeDefault
+    position={[-25, 15, 45]}
+    fov={35}
+    >
+    <OrbitControls
+      enableDamping
+      autoRotate={$orbitRotate}
+      minDistance={40}
+      maxDistance={100}
+    />
+  </T.PerspectiveCamera>
+
+  <T.AmbientLight color="gainsboro" intensity={2}/>
+
+  <PC />
 </Canvas>
