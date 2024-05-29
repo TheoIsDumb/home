858d46716f37744ec4ddc0a3c807e70cba7979ad
theo <lesgooo1993@proton.me>
2024-05-29 16:03:57 +0530

    Revert "removed scene.svelte (enthinn verthe?) moved it all inside app.svelte; pc.blend changes" | reverted cause scene.svelte is actually important lmao, useTask and stuff runs inside a Canvas context, so yeah, reverting
    This reverts commit 39d2aba9a6646b3985049e876573a0a45b6bd955.

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,28 +1,8 @@
 <script>
   import { Canvas } from '@threlte/core'
-
-	import { T } from '@threlte/core'
-	import { OrbitControls } from '@threlte/extras'
-	import PC from './pc.svelte'
-
-	import { orbitRotate } from '$lib/store'
+  import Scene from './Scene.svelte'
 </script>
 
 <Canvas>
-  <T.PerspectiveCamera
-    makeDefault
-    position={[-25, 15, 45]}
-    fov={35}
-    >
-    <OrbitControls
-      enableDamping
-      autoRotate={$orbitRotate}
-      minDistance={40}
-      maxDistance={100}
-    />
-  </T.PerspectiveCamera>
-
-  <T.AmbientLight color="gainsboro" intensity={2}/>
-
-  <PC />
+  <Scene />
 </Canvas>
---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { T } from '@threlte/core'
+	import { OrbitControls } from '@threlte/extras'
+	import PC from './pc.svelte'
+
+	import { orbitRotate } from '$lib/store'
+</script>
+
+<T.PerspectiveCamera
+  makeDefault
+	position={[-25, 15, 45]}
+	fov={35}
+	>
+  <OrbitControls
+		enableDamping
+		autoRotate={$orbitRotate}
+		minDistance={40}
+		maxDistance={100}
+	/>
+</T.PerspectiveCamera>
+
+<T.AmbientLight color="gainsboro" intensity={2}/>
+
+<PC />
