2500ea61a885a7fe6a42fb5a41c8c20dd4750bb6
theo <lesgooo1993@proton.me>
2024-06-05 00:52:54 +0530

    heading.svelte: adjusted arrow size and stuff

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <h1 class="text-amber-100 text-3xl font-bold flex gap-2 items-center">
-    <a href="/">
-        <svg xmlns="http://www.w3.org/2000/svg" class="size-8 inline hover:scale-110"
-        viewBox="0 0 24 24"><path fill="currentColor" d="M14.71 15.88L10.83 12l3.88-3.88a.996.996 0 1 0-1.41-1.41L8.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0c.38-.39.39-1.03 0-1.42"/></svg>
+    <a href="/" class="flex items-center">
+        <svg class="size-4 hover:scale-110"
+        viewBox="0 0 7.6729 12.7" xmlns="http://www.w3.org/2000/svg"><path d="m7.1499 10.754-4.4087-4.4087 4.4087-4.4087a1.1329 1.1329 0 1 0-1.6021-1.6021l-5.2155 5.2155a1.1317 1.1317 0 0 0 0 1.6021l5.2155 5.2155c0.44314 0.44314 1.159 0.44314 1.6021 0 0.43178-0.44314 0.44314-1.1704 0-1.6135" fill="currentColor" stroke-width="1.1363"/></svg>
     </a>
 
     <span>{title}</span>
